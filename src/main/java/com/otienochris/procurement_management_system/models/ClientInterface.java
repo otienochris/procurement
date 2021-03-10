cd9569c82b5ab79662c75523c132bfdf239e44cb
otienochris <otienochris98@gmail.com>
2021-03-10 09:08:19 +0300

    refactored

--- src/main/java/com/otienochris/procurement_management_system/models/ClientInterface.java
+++
@@ -1,4 +0,0 @@
-package com.otienochris.procurement_management_system.models;
-public interface ClientInterface {
-    String getClientId();
-}
--- src/main/java/com/otienochris/procurement_management_system/repositories/TenderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.otienochris.procurement_management_system.repositories;
-
-import com.otienochris.procurement_management_system.models.Tender;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenderRepository extends JpaRepository<Tender, Long> {
-}
