44266bbe75fbbfed47d279cc9b2ef9827eeac9b6
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-23 22:58:17 +0300

    Add files via upload

--- src/main/java/com/otienochris/procurement_management_system/services/OrderManagementService.java
+++ src/main/java/com/otienochris/procurement_management_system/services/OrderManagementService.java
@@ -1,43 +1,88 @@
-package com.groupwork.Explorers.service;
+package com.group4.procurement.service.newones;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.groupwork.Explorers.model.OrderManagement;
+import com.group4.procurement.dao.newones.OrderManagementRepo;
+import com.group4.procurement.model.newones.OrderManagement;
+import com.group4.procurement.model.newones.documents.InvoicesDoc;
+import com.group4.procurement.response.newones.*;
 
+
+
+@Service
 public class OrderManagementService {
-	@Autowired
-	OrderManagement om;
 
-	@RequestMapping("orderaccepted")
-	public void acceptedOrder() {
-		om.setStatus("accepted");
+
+	private OrderManagementRepo ordermanagementrepo;
+	
+	public List<OrderManagement> getAllOrderDoc() {
+		return ordermanagementrepo.findAll();
+	}
+
+	public OrderManagementResponse getOrderDoc(@RequestBody @Validated int orderDocId){
+
+			OrderManagement ordermanagement = ordermanagementrepo.findById(orderDocId).orElseThrow(() -> {throw new NoSuchElementException("The id " + orderDocId + "to this order management doest exist");});
+		return OrderManagementResponse(ordermanagement);
+	}
+
+
+	public OrderManagement saveDoc(String status,
+								   String goodsreceivednote,
+								   String goodsreturnshipment,
+								   MultipartFile invoice) {
+		OrderManagement ordermanagement = null;
+
+		status = ordermanagement.getStatus();
+		goodsreceivednote = ordermanagement.getGoodsReceivedNote();
+		goodsreturnshipment = ordermanagement.getGoodsReturnShipment();
+		invoice = (MultipartFile) ordermanagement.getInvoices();
+
+		ordermanagement = new OrderManagement(status, goodsreceivednote, goodsreturnshipment, (InvoicesDoc) invoice);
+
+		return ordermanagementrepo.save(ordermanagement);
 	}
 	
-	@RequestMapping("orderdenied")
-	public void deniedOrder() {
-		om.setStatus("denied");
+	
+	public void deleteAllDoc() {
+		ordermanagementrepo.deleteAll();
 	}
 	
-	@RequestMapping("grnapproved")
-	public void approvedGRN() {
-		om.setGoodsReceivedNote("approved");
+	
+	public void deleteDoc(int docId) {
+		ordermanagementrepo.delete(ordermanagementrepo.getOne(docId));
 	}
 	
-	@RequestMapping("grnrejected")
-	public void rejectedGRN() {
-		om.setGoodsReceivedNote("rejected");
+
+
+	public OrderManagementResponse OrderManagementResponse(OrderManagement ordermanagement) {
+		InvoicesDoc invoicesdoc = ordermanagement.getInvoices();
+		
+		String invoicesDocName = StringUtils.cleanPath(invoicesdoc.getInvoicesDocName());
+		
+		String invoicesDocPath = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/invoices/document/download/")
+				.path(invoicesDocName)
+				.toUriString();
+		
+		
+		
+		  return OrderManagementResponse.builder() 
+				  .id(ordermanagement.getOrdermanagementId()) 
+				  .invoicesDocUrl(invoicesDocPath)
+				  .build();
+		 
+		
+		
 	}
-	
-	@RequestMapping("grsapproved")
-	public void approvedGRS() {
-		om.setGoodsReturnShipment("approved");
-	}
-	
-	@RequestMapping("grsrejected")
-	public void rejectedGRS() {
-		om.setGoodsReturnShipment("rejected");
-	}	
-	
-	
+
+
 }
