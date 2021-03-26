ae107c7f68e9d1d324726009c580f61c92cb3289
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-26 21:08:19 +0300

    Delete PurchaseRequisitionRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/PurchaseRequisitionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group4.procurement.dao.newones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group4.procurement.model.newones.PurchaseRequisition;
-
-public interface PurchaseRequisitionRepo extends JpaRepository<PurchaseRequisition, Integer> {
-
-}
