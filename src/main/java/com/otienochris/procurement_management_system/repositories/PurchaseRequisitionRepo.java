e0e4932be45a8ce8ca7de752e8156d9931586213
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-26 21:09:16 +0300

    Add files via upload

---
+++ src/main/java/com/otienochris/procurement_management_system/repositories/PurchaseRequisitionRepo.java
@@ -0,0 +1,10 @@
+package com.otienochris.procurement_management_system.repositories;
+import com.otienochris.procurement_management_system.*;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRequisitionRepo extends JpaRepository<PurchaseRequisition, Long> {
+}
---
+++ src/main/java/com/otienochris/procurement_management_system/repositories/RFIRepo.java
@@ -0,0 +1,8 @@
+package com.otienochris.procurement_management_system.repositories;
+import com.otienochris.procurement_management_system.*;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RFIRepo extends JpaRepository<RFI, Long> {
+}
