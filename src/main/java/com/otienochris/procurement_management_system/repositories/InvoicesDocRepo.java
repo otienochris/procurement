ed11d131a52e77cf6d51304b02678770388009df
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-26 21:07:27 +0300

    Delete InvoicesDocRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/InvoicesDocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group4.procurement.dao.newones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group4.procurement.model.newones.documents.InvoicesDoc;
-
-public interface InvoicesDocRepo extends JpaRepository<InvoicesDoc, Integer> {
-
-}
