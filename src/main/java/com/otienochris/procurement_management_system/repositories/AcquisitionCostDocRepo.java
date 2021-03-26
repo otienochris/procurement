1734428af7bea8396bc43e18093fb0528ca799d3
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-26 21:05:42 +0300

    Delete AcquisitionCostDocRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/AcquisitionCostDocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group4.procurement.dao.newones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group4.procurement.model.newones.documents.AcquisitionCostDoc;
-
-public interface AcquisitionCostDocRepo extends JpaRepository<AcquisitionCostDoc, Integer> {
-
-}
