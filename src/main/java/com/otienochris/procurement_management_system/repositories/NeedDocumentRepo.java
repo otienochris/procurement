4dd3967b29a39fe329e69e0054547845b0b9949e
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-26 21:07:54 +0300

    Delete NeedDocumentRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/NeedDocumentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.group4.procurement.dao.newones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.group4.procurement.model.newones.documents.NeedDocument;
-
-public interface NeedDocumentRepo extends JpaRepository<NeedDocument, Integer> {
-
-}
