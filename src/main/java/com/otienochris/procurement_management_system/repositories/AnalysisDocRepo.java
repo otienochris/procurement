55b0a844d209612c21b07cd722546b25fdfb94f8
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-27 10:07:13 +0300

    Delete AnalysisDocRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/AnalysisDocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.otienochris.procurement_management_system.repositories;
-
-import com.otienochris.procurement_management_system.models.AnalysisDoc;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AnalysisDocRepo extends JpaRepository<AnalysisDoc, Integer> {
-
-}
