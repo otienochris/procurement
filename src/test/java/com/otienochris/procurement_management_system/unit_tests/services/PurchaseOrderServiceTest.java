8af13c8ddeffdd54c4d2a7873e44340a6ad1d907
otienochris <otienochris98@gmail.com>
2021-03-27 06:34:13 +0300

    adding tests for the controllers and services

---
+++ src/test/java/com/otienochris/procurement_management_system/unit_tests/services/PurchaseOrderServiceTest.java
@@ -0,0 +1,32 @@
+package com.otienochris.procurement_management_system.unit_tests.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseOrderServiceTest {
+
+    @Test
+    void getPOById() {
+    }
+
+    @Test
+    void getAllPO() {
+    }
+
+    @Test
+    void savePO() {
+    }
+
+    @Test
+    void updatePO() {
+    }
+
+    @Test
+    void deletePO() {
+    }
+
+    @Test
+    void createResponse() {
+    }
+}
