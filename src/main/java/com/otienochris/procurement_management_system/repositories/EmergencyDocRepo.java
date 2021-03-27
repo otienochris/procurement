a15d4626dce56fb50613b18532d6ff34bab7f116
chrizlm <42278574+chrizlm@users.noreply.github.com>
2021-03-27 10:07:30 +0300

    Delete EmergencyDocRepo.java

--- src/main/java/com/otienochris/procurement_management_system/repositories/EmergencyDocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.otienochris.procurement_management_system.repositories;
-
-import com.otienochris.procurement_management_system.models.EmergencyDoc;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmergencyDocRepo extends JpaRepository<EmergencyDoc, Integer> {
-
-}
